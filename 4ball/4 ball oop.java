1f9599540de42c7b1c9e573c469ed0f6edad8046
taekook411 <105535788+taekook411@users.noreply.github.com>
2022-05-31 03:43:17 +0530

    Update 4 ball oop.java

--- 4ball/4 ball oop.java
+++ 4ball/4 ball oop.java
@@ -1,42 +1,55 @@
 import processing.core.PApplet;
-public class TryProcessing extends PApplet{
-	public static final int WIDTH=640;
-	public static final int HEIGHT=400;
-	public static final int DIAMETER=10;
-	int x=0;
-	
-	public static void main(String[] args){PApplet.main("TryProcessing",args);}
-	
-	@Override
-	public void setting(){
-	super.setting();
-	size(WIDTH,HEIGHT);}
-	
-	@Override
-	public void setup(){}
-	
-	@Override
-	public void draw(){
-	paintWhite();
-	drawCircle();}
-	
-	private void drawCircle(){
-	ellipse(x,HEIGHT/5,DIAMETER,DIAMETER);
-	x++;}
-	
-	private void drawCircle(){
-	ellipse(x,HEIGHT2/5,DIAMETER,DIAMETER);
-	x+=2;}
-	
-	private void drawCircle(){
-	ellipse(x,HEIGHT3/5,DIAMETER,DIAMETER);
-	x+=3;}
-	
-	private void drawCircle(){
-	ellipse(x,HEIGHT4/5,DIAMETER,DIAMETER);
-	x+=4;}
-	
-	private void paintWhite(){
-	background(255);}
-	
-	}
+
+public class Main extends PApplet {
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 400;
+
+    int n_of_circles = 4;
+    Circle[] circles = new Circle[n_of_circles];
+
+
+    public static void main(String[] args) {
+
+        PApplet.main("org.example.Main", args);
+    }
+
+   
+    @Override
+    public void settings() {
+        size(WIDTH, HEIGHT);
+        for (int i = 0; i < n_of_circles; i++) {
+            
+            circles[i] = new Circle(1,((i+1)*height/5),i+1);
+        }
+
+    }
+
+    public void draw() {
+        for (int i = 0; i < 4; i++) {
+            circles[i].display();
+            circles[i].move();
+        }
+    }
+
+    class Circle {
+        float startingPoint;
+        float height;
+        float speed;
+
+        Circle(int startingPoint, int height, int speed) {
+            this.startingPoint = startingPoint;
+            this.height = height;
+            this.speed = speed;
+        }
+
+       
+        void display( ) {
+            ellipse(this.startingPoint, this.height, 10, 10);
+        }
+
+        
+        void move(){
+            this.startingPoint = this.startingPoint + this.speed;
+        }
+    }
+}
